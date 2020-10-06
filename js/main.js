2845c0d1379252e40f42d18349a16630f6e5b007
annarldesign <72408224+annarldesign@users.noreply.github.com>
2020-10-06 19:05:31 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Anna.", "Engineer.", "Designer.", "Looking for a job!"],
+      strings: ["Anna.", "Engineer.", "Designer.", "Seeking Employment."],
       typeSpeed: 100,
       loop: true,
     });
